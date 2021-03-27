a4bc995240e061d88b288c9990eb2e4130dd4d39
Bilel REZGUI <bilel.rezgui@wolterskluwer.com>
2021-03-27 12:56:29 +0100

    Add userClient Project

---
+++ demo-pact-brocker-dot-net/UserClient/UserClient.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace userConsumer
+{
+    public class UserClient
+    {
+        private readonly HttpClient _client;
+
+        public UserClient(string baseUri)
+        {
+            _client = new HttpClient { BaseAddress = new Uri(baseUri) };
+        }
+
+        public async Task<string> GetUserMail(int id)
+        {
+            string reasonPhrase;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/api/WeatherForecast/{id}");
+            request.Headers.Add("Accept", "application/json");
+
+            var response = await _client.SendAsync(request);
+
+            var content = await response.Content.ReadAsStringAsync();
+            var status = response.StatusCode;
+
+            reasonPhrase = response.ReasonPhrase;
+
+            request.Dispose();
+            response.Dispose();
+
+            if (status == HttpStatusCode.OK)
+            {
+                var user = !string.IsNullOrEmpty(content) ?
+                   JObject.Parse(content)
+                  : null;
+
+                if (user != null)
+                    return (string)user["email"];
+                else
+                    throw new Exception("user not found.");
+            }
+
+            throw new Exception(reasonPhrase);
+        }
+    }
+}
